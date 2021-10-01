26f362bfd3c71f55b25275044080d391f4565cc1
jpenaa18 <89609904+jpenaa18@users.noreply.github.com>
2021-10-01 13:39:53 -0500

    Update and rename 2021-06 data cleaning to 2021-06 data cleaning.sql

--- 2021-06 data cleaning.sql
+++ 2021-06 data cleaning.sql
@@ -1,5 +1,10 @@
--- Selecting all data from table
+
+/* Data cleaning in PostgreSQL. Keywords used: SELECT, DISTINCT, GROUP BY, WHERE, CASE, UPDATE, ALTER, CREATE, DATE_PART */
+-------------------------------------------------------------------------------------------------------------------
+
+-- Previewing data from table
 SELECT * FROM "202106_tripdata"
+LIMIT 1000
 
 -- Because I'll be deleting rows, I will make a copy of our table. A csv of the file also exists
 SELECT * 
